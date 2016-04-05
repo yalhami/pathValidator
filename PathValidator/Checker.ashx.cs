d47a18d2b838f205ac764de7c14aa037200440c9
yalha <yalha@DESKTOP-GVKK308>
2016-04-05 23:50:53 +0400

    Add project files.

---
+++ PathValidator/Checker.ashx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace PathValidator
+{
+    /// <summary>
+    /// Summary description for Checker
+    /// </summary>
+    public class Checker : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string path = "";
+            if (null == context.Request.Params["filePath"])
+                return;
+            path = context.Request.Params["filePath"];
+
+            if (Directory.Exists(path))
+            {
+                return;
+            }
+            else
+            {
+                int lastExtIndex = 0;
+                lastExtIndex = path.LastIndexOf(".");
+                if (lastExtIndex > 0)
+                {
+                    int lastSlashIndex = 0;
+                    lastSlashIndex = path.LastIndexOf("\\");
+                    path = path.Substring(0, lastSlashIndex);
+                }
+                Directory.CreateDirectory(path);
+            }
+
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
